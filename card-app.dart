77cf6a6ce211cc2a970a871f2d0e7f3666606525
Mithun Adhikari <mithunadhikari40@gmail.com>
2021-04-16 08:14:10 +0545

    enhanced the for loop on deck class

--- card-app.dart
+++ card-app.dart
@@ -1,9 +1,9 @@
 void main () {
   var deck = new Deck();
  
-  print(deck);
+//   print(deck);
   
-//   deck.printCards();
+  deck.printCards();
   
 }
 // this is a collection of cards
@@ -17,15 +17,25 @@ class Deck {
     List<String> suits = ["Spade","Clubs","Hearts","Diamonds"];
     
     // nested for loop
-    for( int i = 0; i< suits.length; i++) {
-      
-      for(int j =0; j< ranks.length; j++ ){
-        //create a new Card and add it to cards
-        var card = new Card(suits[i], ranks[j]);
+//     for( int i = 0; i< suits.length; i++) {
+//       for(int j =0; j< ranks.length; j++ ){
+//         //create a new Card and add it to cards
+//         var card = new Card(suits[i], ranks[j]);
+//         cards.add(card);
+        
+//       }
+//     }
+    for(var suit in suits){
+      for(var rank in ranks){
+        
+        var card = new Card(suit,rank);
         cards.add(card);
         
       }
     }
+//     for(int i= 0;i<suits.length; i++){
+//       var suit = suits[i];
+//     }
   }
   
   void printCards() {
